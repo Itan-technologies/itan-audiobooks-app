91b0c3b5386ae76393cc1933015db40b04a47e38
Ayokunnumi1 <omololuayk@gmail.com>
2025-02-11 17:20:41 +0100

    Update users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  enum role: { author: 'author', listener: 'listener', admin: 'admin' }
+  
+  # Validations
+  validates :first_name, :last_name, :phone_number, :nationality, :role, presence: true
 end
