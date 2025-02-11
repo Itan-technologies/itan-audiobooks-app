0980d6597a14d2dce1177e5d4829d1113923ecb1
Ayokunnumi1 <omololuayk@gmail.com>
2025-02-11 20:49:32 +0100

    Update migration & schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,58 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_11_140129) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_11_194307) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
+
+  create_table "audiobooks", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "book_id", null: false
+    t.string "audio_file_url"
+    t.integer "duration"
+    t.boolean "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_audiobooks_on_book_id"
+  end
+
+  create_table "books", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "user_id", null: false
+    t.string "title"
+    t.text "description"
+    t.string "edition_number"
+    t.string "contributors"
+    t.integer "primary_audience"
+    t.boolean "publishing_rights"
+    t.string "file_url"
+    t.string "cover_image_url"
+    t.integer "status"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_books_on_user_id"
+  end
+
+  create_table "chapters", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "audiobook_id", null: false
+    t.string "title"
+    t.string "audio_file_url"
+    t.uuid "duration"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["audiobook_id"], name: "index_chapters_on_audiobook_id"
+  end
+
+  create_table "reviews", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "user_id", null: false
+    t.uuid "audiobook_id", null: false
+    t.integer "ratings"
+    t.text "comments"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["audiobook_id"], name: "index_reviews_on_audiobook_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
 
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -31,4 +79,10 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_11_140129) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "audiobooks", "books"
+  add_foreign_key "books", "users"
+  add_foreign_key "chapters", "audiobooks"
+  add_foreign_key "reviews", "audiobooks"
+  add_foreign_key "reviews", "users"
 end
