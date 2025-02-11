7485a90b991ee115889ec111d4e5aa7440b1ab47
Ol-create <oluola96@gmail.com>
2025-02-11 15:34:04 +0100

    add jwt middleware

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,11 @@ module ItanAudiobooksApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
+    config.api_only = true
+
+    # Middleware for Devise JWT
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
